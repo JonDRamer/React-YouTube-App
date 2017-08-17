db25fc64bfdfc52012b1c1f7dd5f6bf4115696d2
Jon Ramer <jramer87@gmail.com>
2017-08-17 16:05:35 -0500

    Updates state to SearchBar component anytime something new is typed in the search box

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDmsAUgCdfm_LASXEn5-qHshKiOq3EzQZo';
 
 // Create a new component & this component should produce some HTML
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this components generated HTML and put it on the page (in the DOM)
